abdcfb724052e1ed8a3ae28f60a1bb1f352bd083
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-10-28 08:19:58 +0100

    Add pyzed to Pipfile.

---
+++ src/test_opencv.py
@@ -0,0 +1,8 @@
+import cv2
+
+def main():
+	print(cv2.__version__)
+	print(cv2.__path__)
+
+if __name__ == '__main__':
+	main()
---
+++ src/test_zed.py
@@ -0,0 +1,7 @@
+import pyzed.sl as sl
+
+def main():
+	print("ZED SDK imported successfully!")
+
+if __name__ == '__main__':
+	main()
