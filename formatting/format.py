6ca43d69e9f9b1c61e5774e0c47babed4f77830a
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2020-10-29 16:37:47 +0100

    Add timestamps, unrectified image and camera information to SVO format.

--- formatting/format.py
+++ formatting/format.py
@@ -8,42 +8,37 @@ import pyzed.sl as sl
 from utilities import progress_bar
 
 class Format(enum.Enum):
-	ORBSlam = 1
+	Default = 1
 
 def format_from_string(string: str) -> Format:
-	if string == "ORBSlam":
-		return Format.ORBSlam
+	if string == "Default":
+		return Format.Default
 	else:
 		return None
 
-class ORBSlamFormatter:
-	def __init__(self, svo_file, output_root):
-		self.input = svo_file
-		self.output = dict()
-		self.output['root'] = output_root
-
-	def validate_input(self):
-		if not os.path.exists(self.input):
+class SVOFormatter:
+	def __init__(self, svo_file: str, output_dir=None, fill=8):
+		if not os.path.exists(svo_file):
 			raise Exception('Input file does not exists.')
-		if os.path.splitext(self.input)[-1].lower() != ".svo":
+		if os.path.splitext(svo_file)[-1].lower() != ".svo":
 			raise Exception('Input file is not a SVO file.')
 
-	def validate_output(self):
-		if not os.path.exists(self.output['root']):
-			raise Exception('Output folder does not exist.')
+		if output_dir != None:
+			root = output_dir
+		else:
+			root = "".join(list(os.path.split(svo_file)[:-1]))
 
-		self.output['left'] = self.output['root'] + '/left'
-		self.output['right'] = self.output['root'] + '/right'
-		
-		os.makedirs(self.output['left'], exist_ok=True)
-		os.makedirs(self.output['right'], exist_ok=True)
+		self.svo_file = svo_file
+		self.root = root + '/' \
+			+ os.path.splitext(os.path.split(svo_file)[-1])[0]
+		os.makedirs(self.root)
+
+		print("{:15} {:>}".format("SVO Input: ", self.svo_file))
+		print("{:15} {:>}".format("SVO Output: ", self.root))
 
 	def format(self):
-		self.validate_input()
-		self.validate_output()
-		
 		init_params = sl.InitParameters()
-		init_params.set_from_svo_file(self.input)
+		init_params.set_from_svo_file(self.svo_file)
 		init_params.svo_real_time_mode = False
 		init_params.coordinate_units = sl.UNIT.MILLIMETER
 
@@ -54,39 +49,149 @@ class ORBSlamFormatter:
 			sys.stdout.write(repr(error))
 			zed.close()
 			exit()
+		
+		# Camera information.
+		information_formatter = InformationFormatter(self.root, zed)
+		information_formatter.format()
 
+		# Images.
+		image_formatter = ImageFormatter(self.root, zed)
+		image_formatter.format()
+
+		
+
+		zed.close()
+
+class InformationFormatter(object):
+	def __init__(self, root: str, camera: sl.Camera):
+		self.root = root
+		self.info = camera.get_camera_information()		
+		self.extrinsics = self.info.calibration_parameters_raw
+		self.left_intrinsics = \
+			self.info.calibration_parameters_raw.left_cam
+		self.right_intrinsics = \
+			self.info.calibration_parameters_raw.right_cam
+
+	def format(self):
+		info_file_path = self.root + '/camera_information.txt'
+		info_file = open(info_file_path, "w")
+		info_file.write("Camera\n")
+		info_file.write("{:15s} {:>10s}\n".format(
+			"Model: ",
+			self.info.camera_model))
+		info_file.write("{:15s} {:10d}\n".format(
+			"Serial no.: ",
+			self.info.serial_number))
+		info_file.write("{:15s} {:10d}\n".format(
+			"Image width: ",
+			self.info.camera_resolution.width))
+		info_file.write("{:15s} {:10d}\n".format(
+			"Image height: ",
+			self.info.camera_resolution.height))
+		info_file.write("{:15s} {:10.0f}\n".format(
+			"FPS: ",
+			self.info.camera_fps))
+
+		info_file.write("\nExtrinsics\n")
+		info_file.write("Rotation: \n{0}\n".format(
+			self.extrinsics.R))
+		info_file.write("Translation: \n{0}\n".format(
+			self.extrinsics.T))
+		
+		info_file.write("\nIntrinsics - left\n")
+		info_file.write("fx: {0}\n".format(self.left_intrinsics.fx))
+		info_file.write("fy: {0}\n".format(self.left_intrinsics.fy))
+		info_file.write("cx: {0}\n".format(self.left_intrinsics.cx))
+		info_file.write("cy: {0}\n".format(self.left_intrinsics.cy))
+		info_file.write("distortion: \n{0}\n".format(
+			self.left_intrinsics.disto))
+
+		info_file.write("\nIntrinsics - right\n")
+		info_file.write("fx: {0}\n".format(self.right_intrinsics.fx))
+		info_file.write("fy: {0}\n".format(self.right_intrinsics.fy))
+		info_file.write("cx: {0}\n".format(self.right_intrinsics.cx))
+		info_file.write("cy: {0}\n".format(self.right_intrinsics.cy))
+		info_file.write("distortion: \n{0}\n".format(
+			self.right_intrinsics.disto))
+		
+		info_file.close()
+
+class ImageFormatter(object):
+	def __init__(self, root: str, camera: sl.Camera, fill: int=8):
+		self.root = root
+		self.fill = fill
+		self.camera = camera
+
+		self.paths = dict()
+		self.paths['left'] = self.root + '/left'
+		self.paths['right'] = self.root + '/right'
+		self.paths['left_unrectified'] = self.root \
+			+ '/left_unrectified'
+		self.paths['right_unrectified'] = self.root \
+			+ '/right_unrectified'
+			
+		os.makedirs(self.paths['left'], exist_ok=True)
+		os.makedirs(self.paths['right'], exist_ok=True)
+		os.makedirs(self.paths['left_unrectified'], exist_ok=True)
+		os.makedirs(self.paths['right_unrectified'], exist_ok=True)
+	
+	def format(self):
+		# Image and timestamp conversion.
 		left_image = sl.Mat()
+		left_unrectified_image = sl.Mat()
 		right_image = sl.Mat()
+		right_unrectified_image = sl.Mat()
 		timestamp = sl.Timestamp()
 
 		runtime_params = sl.RuntimeParameters()
 		runtime_params.sensing_mode = sl.SENSING_MODE.FILL
-		n_frames = zed.get_svo_number_of_frames()
+		n_frames = self.camera.get_svo_number_of_frames()
 
 		sys.stdout.write('Converting SVO...\n')
+		times_file = open(self.root + '/times.txt',"w+")
 		while True:
-			if zed.grab(runtime_params) != sl.ERROR_CODE.SUCCESS:
+			if self.camera.grab(runtime_params) \
+				!= sl.ERROR_CODE.SUCCESS:
 				continue
 		
-			svo_position = zed.get_svo_position()
-			zed.retrieve_image(left_image, sl.VIEW.LEFT)
-			zed.retrieve_image(right_image, sl.VIEW.RIGHT)
-			timestamp = zed.get_timestamp(sl.TIME_REFERENCE.IMAGE)
-			milliseconds = timestamp.get_milliseconds()
-			
-			left_filename = self.output['left'] + '/' \
-				+ 'left_{0}.png'.format(milliseconds)
-			right_filename = self.output['right'] + '/' \
-				+ 'right_{0}.png'.format(milliseconds)
+			svo_position = self.camera.get_svo_position()
+			self.camera.retrieve_image(left_image, sl.VIEW.LEFT)
+			self.camera.retrieve_image(right_image, sl.VIEW.RIGHT)
+			self.camera.retrieve_image(left_unrectified_image, 
+				sl.VIEW.LEFT_UNRECTIFIED)
+			self.camera.retrieve_image(right_unrectified_image, 
+				sl.VIEW.RIGHT_UNRECTIFIED)
 
+			timestamp = self.camera.get_timestamp(
+				sl.TIME_REFERENCE.IMAGE)
+			milliseconds = timestamp.get_milliseconds()
+		
+			file_name = '{0}'.format(svo_position) \
+				.zfill(self.fill) + '.png'
 				
-			cv2.imwrite(left_filename, left_image.get_data())
-			cv2.imwrite(right_filename, right_image.get_data())
+			left_image_file = \
+				self.paths['left'] + '/' + file_name
+			left_unrectified_image_file = \
+				self.paths['left_unrectified'] + '/' \
+				+ file_name
+			right_image_file = \
+				self.paths['right'] + '/' + file_name
+			right_unrectified_image_file = \
+				self.paths['right_unrectified'] + '/' \
+				+ file_name
+
+			# Write to files.
+			cv2.imwrite(left_image_file, left_image.get_data())
+			cv2.imwrite(right_image_file, right_image.get_data())
+			cv2.imwrite(left_unrectified_image_file, 
+				left_unrectified_image.get_data())
+			cv2.imwrite(right_unrectified_image_file, 
+				right_unrectified_image.get_data())
+
+			times_file.write(str(milliseconds) + '\n')
 
 			progress_bar((svo_position + 1) / n_frames * 100, 60)
 			
 			if svo_position >= (n_frames - 1):
 				sys.stdout.write("\nSVO conversion ended.\n")
 				break
-
-		zed.close()
--- formatting/format_svo.py
+++ formatting/format_svo.py
@@ -3,11 +3,11 @@ import cv2
 import pyzed.sl as sl
 import sys
 
-from format import Format, format_from_string, ORBSlamFormatter
+from format import Format, format_from_string, SVOFormatter
 
-def format_svo(svo_file: str, output_dir: str, format: Format):
-	if format == Format.ORBSlam:
-		formatter = ORBSlamFormatter(svo_file, output_dir)
+def format_svo(svo_file: str, format: Format):
+	if format == Format.Default:
+		formatter = SVOFormatter(svo_file)
 		formatter.format()
 
 def main():
@@ -15,13 +15,11 @@ def main():
 		to the specified format.')
 	parser.add_argument('-i', type=str, help='input file', 
 		required=True)
-	parser.add_argument('-o', type=str, help='output root directory', 
-		required=True)
 	parser.add_argument('-f', type=str, help='output format',
-		default="ORBSlam", choices=["ORBSlam"])
+		default="Default", choices=["Default"])
 	args = parser.parse_args()
 
-	format_svo(args.i, args.o, format_from_string(args.f))
+	format_svo(args.i, format_from_string(args.f))
 
 if __name__ == '__main__':
 	main()
