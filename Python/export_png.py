81c424a4c699bc2ec29d4333f95291e3a9b23821
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2021-05-18 12:37:35 +0200

    Add start, stop and skip variables to PNG export function.

--- Python/export_png.py
+++ Python/export_png.py
@@ -7,7 +7,8 @@ import pyzed.sl as sl
 
 from utilities import progress_bar
 
-def export_png(svo_file: str, output_dir: str):
+def export_png(svo_file: str, output_dir: str, start_frame: int, \
+    stop_frame: int, skip_frame: int):
     assert os.path.exists(svo_file), "Input file does not exist."
     assert os.path.splitext(svo_file)[-1] == ".svo", \
         "Input file is not a SVO file."
@@ -33,28 +34,36 @@ def export_png(svo_file: str, output_dir: str):
         zed.close()
         exit()
 
-    print()
-    print("Export from: {0}".format(svo_file))
-    print("Export to:   {0}".format(output_dir))
-
     # Get SVO information and set SVO position.
     camera_info = zed.get_camera_information()
     frames_total = zed.get_svo_number_of_frames()
+    zed.set_svo_position(start_frame)
+
+    if stop_frame > frames_total:
+        stop_frame = frames_total
 
     # Allocate.
     left_image = sl.Mat()
     right_image = sl.Mat()
 
+    print()
+    print("Export from: {0}".format(svo_file))
+    print("Export to:   {0}".format(output_dir))
+    print("Frames:      {0}-{1}".format(start_frame, stop_frame))
+
     running = True
     frame = 0
-    while frame < frames_total:
+    while frame <= stop_frame and frame < frames_total:
         if zed.grab() != sl.ERROR_CODE.SUCCESS:
             continue
 
         frame = zed.get_svo_position()
 
+        if (frame - start_frame) % skip_frame != 0:
+            continue
+
         # Show progress bar.
-        progress_bar(frame / frames_total)
+        progress_bar((frame - start_frame) / (stop_frame - start_frame) * 100)
 
         # Get SVO images and timestamp.
         zed.retrieve_image(left_image, sl.VIEW.LEFT_UNRECTIFIED)
@@ -74,11 +83,14 @@ def export_png(svo_file: str, output_dir: str):
 def main():
     parser = argparse.ArgumentParser(description="Export the images from a \
         SVO file as PNG files.")
-    parser.add_argument("-input", type=str, help="Input SVO file.")
-    parser.add_argument("-output", type=str, help="Output directory.")
+    parser.add_argument("--input", type=str, help="Input SVO file.")
+    parser.add_argument("--output", type=str, help="Output directory.")
+    parser.add_argument("--start", type=int, help="Start index.")
+    parser.add_argument("--stop", type=int, help="Stop index.")
+    parser.add_argument("--skip", type=int, help="Index skip.")
     args = parser.parse_args()
 
-    export_png(args.input, args.output)
+    export_png(args.input, args.output, args.start, args.stop, args.skip)
 
 if __name__ == "__main__":
     main()
